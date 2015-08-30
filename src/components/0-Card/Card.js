34c9cd17fb46ec7659b214a220ce961c8f9f9268
Hugh Evans <hugh@hughevans.net>
2015-08-30 13:26:52 +1000

    Animate new cards in on an interval

---
+++ src/components/0-Card/Card.js
@@ -0,0 +1,16 @@
+import styles from './Card.css';
+
+import React, { Component } from 'react';
+
+export default class Card extends Component {
+
+  render() {
+    return (
+      <div className={styles.card}>
+        <img src="https://placehold.it/350x150" />
+        <div>{ this.props.caption }</div>
+      </div>
+    );
+  }
+
+};
--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,51 @@ import styles from './App.css';
 
 import React, { Component } from 'react';
 
+import Card from './0-Card/Card';
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.startFreefall();
+  }
+
+  state = {
+    cards: [],
+  }
+
+  startFreefall() {
+    this.freefallTimer = setInterval(this.addCard, 3000);
+  }
+
+  addCard = () => {
+    let cards = this.state.cards;
+    let newCardNum = 1;
+
+    if (this.state.cards.length > 0) {
+      newCardNum = this.state.cards[this.state.cards.length - 1].cardNum + 1;
+    }
+
+    cards.push({
+      caption: 'Caption',
+      cardNum: newCardNum,
+    });
+
+    if (this.state.cards.length > 20) { cards.shift() }
+
+    this.setState({ cards: cards });
+  }
+
+  cards() {
+    return this.state.cards.map((card) => {
+      return <Card key={ card.cardNum } caption= { card.cardNum + '. ' + card.caption } />;
+    });
+  }
+
   render() {
     return (
-      <h1 className={ styles.heading }>
-        Hello,&nbsp;
-        <span className={ styles.world }>World</span>
-      </h1>
+      <div className={ styles.canvas }>
+        { this.cards() }
+      </div>
     );
   }
 }
