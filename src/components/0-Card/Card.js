78e62c89c7bdbd7ad5d552bea27534b4602535f2
Hugh Evans <hugh@hughevans.net>
2015-08-30 15:41:47 +1000

    Some more card animation

--- src/components/0-Card/Card.js
+++ src/components/0-Card/Card.js
@@ -7,7 +7,7 @@ export default class Card extends Component {
   render() {
     return (
       <div className={styles.card}>
-        <img src="https://placehold.it/350x150" />
+        <img src="https://placehold.it/350x350" />
         <div>{ this.props.caption }</div>
       </div>
     );
