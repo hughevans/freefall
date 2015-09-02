0bca812660fc8224018e17a3424ea3ab0939538f
Hugh Evans <hugh@hughevans.net>
2015-09-02 21:41:38 +1000

    Randomise the initial position

--- src/components/0-Card/Card.js
+++ src/components/0-Card/Card.js
@@ -4,9 +4,25 @@ import React, { Component } from 'react';
 
 export default class Card extends Component {
 
+  state = {
+    x: this.randomPosition(),
+    y: this.randomPosition(),
+  }
+
+  cardStyle() {
+    return {
+      left: this.state.x + '%',
+      top: this.state.y + '%',
+    };
+  }
+
+  randomPosition() {
+    return Math.floor(Math.random() * 1000) / 10;
+  }
+
   render() {
     return (
-      <div className={ styles.card }>
+      <div style={ this.cardStyle() } className={ styles.card }>
         <img src="https://placehold.it/350x350" />
         <div className= { styles.caption }>{ this.props.caption }</div>
       </div>
