da65e856043b2aa0918659ee79841c2c64faba70
Hugh Evans <hugh@hughevans.net>
2015-09-03 21:21:04 +1000

    Randomise the final rotation

--- src/components/0-Card/Card.js
+++ src/components/0-Card/Card.js
@@ -7,6 +7,7 @@ export default class Card extends Component {
   state = {
     width: this.randomSize(),
     height: this.randomSize(),
+    rotation: this.randomRotation(),
     x: this.randomPosition(),
     y: this.randomPosition(),
   }
@@ -15,6 +16,7 @@ export default class Card extends Component {
     return {
       left: this.state.x + '%',
       top: this.state.y + '%',
+      transform: 'rotate(' + this.state.rotation + 'deg)',
     };
   }
 
@@ -26,6 +28,10 @@ export default class Card extends Component {
     return Math.floor(Math.random() * 1000) / 10;
   }
 
+  randomRotation() {
+    return (Math.floor(Math.random() * 600) / 10) - 30;
+  }
+
   imageSrc() {
     return 'https://placehold.it/' + this.state.width + 'x' + this.state.height;
   }
@@ -33,8 +39,8 @@ export default class Card extends Component {
   render() {
     return (
       <div style={ this.cardStyle() } className={ styles.card }>
-        <img src={ this.imageSrc() } height={ this.state.height} width={this.state.width} />
-        <div className= { styles.caption }>{ this.props.caption }</div>
+        <img src={ this.imageSrc() } height={ this.state.height} width={ this.state.width } />
+        <div className={ styles.caption }>{ this.props.caption }</div>
       </div>
     );
   }
