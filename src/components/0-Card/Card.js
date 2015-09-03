7c1b1f83c46a958a252adbfe0ae3e64de9e8321d
Hugh Evans <hugh@hughevans.net>
2015-09-03 20:20:34 +1000

    Randomise card size

--- src/components/0-Card/Card.js
+++ src/components/0-Card/Card.js
@@ -5,6 +5,8 @@ import React, { Component } from 'react';
 export default class Card extends Component {
 
   state = {
+    width: this.randomSize(),
+    height: this.randomSize(),
     x: this.randomPosition(),
     y: this.randomPosition(),
   }
@@ -16,14 +18,22 @@ export default class Card extends Component {
     };
   }
 
+  randomSize() {
+    return Math.floor(Math.random() * 300) + 100;
+  }
+
   randomPosition() {
     return Math.floor(Math.random() * 1000) / 10;
+  }
+
+  imageSrc() {
+    return 'https://placehold.it/' + this.state.width + 'x' + this.state.height
   }
 
   render() {
     return (
       <div style={ this.cardStyle() } className={ styles.card }>
-        <img src="https://placehold.it/350x350" />
+        <img src={ this.imageSrc() } height={ this.state.height} width={this.state.width} />
         <div className= { styles.caption }>{ this.props.caption }</div>
       </div>
     );
