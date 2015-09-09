97dd9898c2d660eb85e2475ddba4031535bd3061
Hugh Evans <hugh@hughevans.net>
2015-09-09 21:55:13 +1000

    Add the ability to drag the cards around

--- src/components/0-Card/Card.js
+++ src/components/0-Card/Card.js
@@ -8,9 +8,61 @@ export default class Card extends Component {
   }
 
   state = {
+    dragging: false,
+    pos: {
+      x: this.determineX(),
+      y: this.determineY(),
+    },
+    rel: null,
     rotation: this.randomRotation(),
-    x: this.determineX(),
-    y: this.determineY(),
+  }
+
+  componentDidUpdate(props, state) {
+    if (this.state.dragging && !state.dragging) {
+      document.addEventListener('mousemove', this.onMouseMove);
+      document.addEventListener('mouseup', this.onMouseUp);
+    } else if (!this.state.dragging && state.dragging) {
+      document.removeEventListener('mousemove', this.onMouseMove);
+      document.removeEventListener('mouseup', this.onMouseUp);
+    }
+  }
+
+  onMouseDown = (e) => {
+    this.activateCard();
+
+    if (e.button !== 0) return;
+
+    this.setState({
+      dragging: true,
+      rel: {
+        x: e.pageX - (this.state.pos.x / 100 * window.screen.width),
+        y: e.pageY - (this.state.pos.y / 100 * window.screen.height),
+      },
+    });
+
+    e.stopPropagation();
+    e.preventDefault();
+  }
+
+  onMouseUp = (e) => {
+    this.setState({dragging: false});
+
+    e.stopPropagation();
+    e.preventDefault();
+  }
+
+  onMouseMove = (e) => {
+    if (!this.state.dragging) return;
+
+    this.setState({
+      pos: {
+        x: 100 / window.screen.width * (e.pageX - this.state.rel.x),
+        y: 100 / window.screen.height * (e.pageY - this.state.rel.y),
+      }
+    });
+
+    e.stopPropagation();
+    e.preventDefault();
   }
 
   determineX() {
@@ -27,8 +79,8 @@ export default class Card extends Component {
 
   cardStyle() {
     return {
-      left: this.state.x + '%',
-      top: this.state.y + '%',
+      left: this.state.pos.x + '%',
+      top: this.state.pos.y + '%',
       transform: 'rotate(' + this.state.rotation + 'deg)',
       zIndex: this.props.card.zIndex,
     };
@@ -38,14 +90,13 @@ export default class Card extends Component {
     return (Math.pow(Math.random(), 2) * 300 / 10) - (Math.pow(Math.random(), 2) * 300 / 10);
   }
 
-  activateCard = (e) => {
-    e.preventDefault();
+  activateCard() {
     this.props.activateCard(this.props.index);
   }
 
   render() {
     return (
-      <a style={ this.cardStyle() } className={ styles.card } onMouseDown={ this.activateCard }>
+      <a ref="card" style={ this.cardStyle() } className={ styles.card } onMouseDown={ this.onMouseDown }>
         <img src={ this.props.card.src } height={ this.props.card.height} width={ this.props.card.width } />
       </a>
     );
